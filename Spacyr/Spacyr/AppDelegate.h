16b4c8bcfa054aa4956b06a9edfc4c59c515dbcb
Daniel Muraveyko <muraveyko_d@mail.ru>
2017-05-26 14:59:23 +0300

    Empty project….

---
+++ Spacyr/Spacyr/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Spacyr
+//
+//  Created by Daniel Muraveyko on 26/05/2017.
+//  Copyright © 2017 Daniel Muraveyko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Spacyr/Spacyr/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Spacyr
+//
+//  Created by Daniel Muraveyko on 26/05/2017.
+//  Copyright © 2017 Daniel Muraveyko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
